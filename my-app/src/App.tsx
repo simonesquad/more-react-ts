b38a6901fad1780acbc89fb7862da5f43a71ef81
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-23 13:26:04 -0500

    fixed minor state mgmt bug

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
 
   const todoDeleteHandler = (todoId: string) => {
     setTodos(prevTodos => {
-      return prevTodos.filter(todo => todo.id === todoId);
+      return prevTodos.filter(todo => todo.id !== todoId);
     });
   };
 
